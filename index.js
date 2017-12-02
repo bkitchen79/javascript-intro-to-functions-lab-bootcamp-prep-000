51fad10d73a01a05495ef62d2d0de2472875ae30
Bradley Kitchen <bradkitchen2004@sbcglobal.net>
2017-12-02 05:07:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,16 +19,7 @@ function SayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
   return "I can't hear you!"
   }
-}
 
-function SayHiToGrandma(string) {
-  if (string.toUpperCase() === string) {
-  return "YES INDEED!"
-  }
-}
-
-function SayHiToGrandma(string) {
-  return "I love you, too."
 }
 
 
