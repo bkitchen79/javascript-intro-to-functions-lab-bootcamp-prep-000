f24219a274cb69275a3a7b0c54238cb4e36a6673
Bradley Kitchen <bradkitchen2004@sbcglobal.net>
2017-12-02 05:11:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
   console.log("hello")
 }
 
-function SayHiToGrandma(string) {
+function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
